b7c8b4edd8e21ef73861d0f3bf5cb2b833e97a2c
Avisrara74 <avisrara74@gmail.com>
2019-04-05 13:18:06 +0400

    upd

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>socksDesign</title>
+	<link rel="stylesheet" href="styles/style.css" type="text/css">
 </head>
 <body>
 	<header>
@@ -17,12 +18,59 @@
 			<li>Оплата</li>
 		</ul>
 	</header>
+
+
 	<main>
-		
+	<!-- main theme -->
+		<section>
+			<h1>Цветные,<br>яркие,<br>твои!</h1>
+			<p>- оригинальные принты и надписи</p>
+			<p>- подарочная упаковка</p>
+			<p>- сделано в России</p>
+		</section>
+
+	<!-- magazine items -->
+		<section>
+			<h2>Магазин</h2>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</section>
+
+	<!-- recomendation -->
+		<section>
+			<div><h3></h3></div>
+			<div><h3></h3></div>
+			<div><h3></h3></div>
+			<div><h3></h3></div>
+		</section>
+
+	<!-- some info -->
+		<section>
+			<div>
+				<img src="#" alt="test">
+				<p>monday, october 22</p>
+				<p>concept animals</p>
+				<p>они все знают</p>
+			</div>
+		</section>
 	</main>
+
+
 	<footer>
 		<div>BBBSocks</div>
-		<div><span>+7 982 325 70 59</span></div>
+		<div>
+			<span>+7 982 325 70 59</span>
+			<ul>
+				<li>О нас</li>
+				<li>Доставка</li>
+				<li>Оплата</li>
+				<li>Возврат</li>
+			</ul>
+		</div>
 		<div>
 			<span>Политика конфиденциальности<br>avisrara74@gmail.com<br></span>
 			<span>SocksDesign, 2019</span>
