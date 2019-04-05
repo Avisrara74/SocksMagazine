08a7a6afa638afa1d3ba9e7ff5e04bab985a76fe
Avisrara74 <avisrara74@gmail.com>
2019-04-05 14:49:09 +0400

    header complite

--- index.php
+++ index.php
@@ -4,26 +4,36 @@
 	<meta charset="UTF-8">
 	<title>socksDesign</title>
 	<link rel="stylesheet" href="styles/style.css" type="text/css">
+	<link rel="shortcut icon" href="">
 </head>
 <body>
 	<header>
-		<div class="logo">
-			<img src="#" alt="Логотип">
-		</div>
-		<ul>
-			<li>Главная</li>
-			<li>Магазин</li>
-			<li>О нас</li>
-			<li>Доставка</li>
-			<li>Оплата</li>
-		</ul>
+		<a href="#" class="link-style-fix">
+			<div class="logo">
+				<img src="images/main/logo.png" alt="Логотип">
+				<span>Big Bang Socks</span>
+			</div>
+		</a>
+		<nav>
+			<ul>
+				<li><a href="#">Главная</a></li>
+				<span>/</span>
+				<li><a href="#">Магазин</a></li>
+				<span>/</span>
+				<li><a href="#">О нас</a></li>
+				<span>/</span>
+				<li><a href="#">Доставка</a></li>
+				<span>/</span>
+				<li><a href="#">Оплата</a></li>
+			</ul>
+		</nav>
 	</header>
 
 
 	<main>
 	<!-- main theme -->
 		<section>
-			<h1>Цветные,<br>яркие,<br>твои!</h1>
+			<h2>Цветные,<br>яркие,<br>твои!</h2>
 			<p>- оригинальные принты и надписи</p>
 			<p>- подарочная упаковка</p>
 			<p>- сделано в России</p>
