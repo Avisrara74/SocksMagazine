1755eb7951a1d6c59f3a6e9e04885c70a6a657ac
Avisrara74 <avisrara74@gmail.com>
2019-04-05 12:42:02 +0400

    projectStart

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>socksDesign</title>
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<img src="#" alt="Логотип">
+		</div>
+		<ul>
+			<li>Главная</li>
+			<li>Магазин</li>
+			<li>О нас</li>
+			<li>Доставка</li>
+			<li>Оплата</li>
+		</ul>
+	</header>
+	<main>
+		
+	</main>
+	<footer>
+		<div>BBBSocks</div>
+		<div><span>+7 982 325 70 59</span></div>
+		<div>
+			<span>Политика конфиденциальности<br>avisrara74@gmail.com<br></span>
+			<span>SocksDesign, 2019</span>
+		</div>
+	</footer>
+</body>
+</html>
