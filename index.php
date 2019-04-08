95fcbea30456d92bfe0f42dbc40e2c506f81a33e
Avisrara74 <avisrara74@gmail.com>
2019-04-08 11:40:56 +0400

    complite

--- index.php
+++ index.php
@@ -33,38 +33,90 @@
 	<!-- main theme -->
 		<section class="mainTheme">
 			<h2>Цветные,<br>яркие,<br>твои!</h2>
+			<br>
 			<p>- оригинальные принты и надписи</p>
+			<br>
 			<p>- подарочная упаковка</p>
+			<br>
 			<p>- сделано в России</p>
 		</section>
 
 	<!-- magazine items -->
 		<section class="navItems">
 			<h2>Магазин</h2>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div class="navItemsWrap">
+				<a href="#"><div class="content-img"><h3>Наборы носков</h3></div></a>
+				<a href="#"><div class="content-img"><h3>Носки стандартной <br> длинны</h3></div></a>
+				<a href="#"><div class="content-img"><h3>Укороченные носки</h3></div></a>
+				<a href="#"><div class="content-img"><h3>Утепленные носки</h3></div></a>
+				<a href="#"><div class="content-img"><h3>Спортивные носки</h3></div></a>
+				<a href="#"><div class="content-img"><h3>Подарочные коробки</h3></div></a>
+			</div>
 		</section>
 
 	<!-- recomendation -->
 		<section class="recomendationItems">
-			<div><h3></h3></div>
-			<div><h3></h3></div>
-			<div><h3></h3></div>
-			<div><h3></h3></div>
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>	
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>	
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>	
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>	
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>	
+			<div>
+				<a href="#">
+					<img src="images/main/1.jpg" alt="test"><br>
+					<p>monday, october 22</p><br>
+					<p>concept animals</p><br>
+					<p>они все знают</p>
+				</a>
+			</div>		
 		</section>
 
 	<!-- some info -->
 		<section class="mainPageSomeInfo">
 			<div>
-				<img src="#" alt="test">
-				<p>monday, october 22</p>
-				<p>concept animals</p>
-				<p>они все знают</p>
+				<h5>BIG BANG SOCKS - НОСКИ БОЛЬШОГО ВЗРЫВА</h5>
+				<h5>Бескрайний космос, наша планета, человек — все это появилось в результате одного неординарного события, которое ученые называют «большим взрывом».</h5>
+				<h5>Большой взрыв — это творческий импульс, из которого родилось «всё» буквально из «ничего».</h5>
+				<h5>Так же было и с нашими носками. Они появились из ничего в результате большого взрыва мозга их создателей.</h5>
+				<h5>Теперь мы хотим, чтобы наши яркие и нескучные носки вдохновляли воплощать самые смелые замыслы.</h5>
+				<h5>Создай свою Вселенную вместе с нами!</h5>
 			</div>
+			<div><img src="images/main/8.png" alt=""></div>
 		</section>
 	</main>
 
