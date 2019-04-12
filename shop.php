1040813e44f028d9881c1748ab2ad18eb60d351f
Avisrara74 <avisrara74@gmail.com>
2019-04-12 13:36:38 +0400

    need fix increase

---
+++ shop.php
@@ -0,0 +1,41 @@
+<main class="mainShop">
+	<div class="shopItems">
+		<a href="#">
+			<div>
+				<h4>Наборы носков</h4>
+			</div>
+		</a>
+		<a href="#">
+			<div>
+				<h4>Носки стандартной <br> длинны</h4>
+			</div>
+		</a>
+		<a href="#">
+			<div>
+				<h4>Укороченные носки</h4>
+			</div>
+		</a>
+		<a href="#">
+			<div class="increase">
+				<h4>Утепленные носки</h4>
+			</div>
+		</a>
+		<a href="#">
+			<div>
+				<h4>Спортивные носки</h4>
+			</div>
+		</a>
+		<a href="#">
+			<div>
+				<h4>Подарочные коробки</h4>
+			</div>
+		</a>
+	</div>
+	
+
+	<div class="bottomButtons">
+		  <a href="/index.php?page=search" class="searchLink"><div class="searchButton"><img src="/images/shop/search.png" title="Поиск"><p>Поиск</p></div></a>
+		  <a href="#" class="basketLink"><div class="basketButton"><img src="/images/shop/shop-basket.png" title="Корзина"><p>Корзина</p></div></a>
+	</div>
+
+</main>
