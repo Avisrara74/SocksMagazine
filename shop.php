b6c3d09bf10d45bf5815698d8ea8342ac17b5e33
avisrara74 <avisrara74@gmail.com>
2019-05-04 18:46:50 +0500

    categoryComplite

--- shop.php
+++ shop.php
@@ -1,32 +1,32 @@
 <main class="mainShop">
 	<div class="shopItems">
 		<a href="index.php?page=Наборы носков">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Наборы носков</h4>
 			</div>
 		</a>
 		<a href="index.php?page=Носки стандартной длинны">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Носки стандартной <br> длинны</h4>
 			</div>
 		</a>
 		<a href="index.php?page=Укороченные носки">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Укороченные носки</h4>
 			</div>
 		</a>
 		<a href="index.php?page=Утепленные носки">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Утепленные носки</h4>
 			</div>
 		</a>
 		<a href="index.php?page=Спортивные носки">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Спортивные носки</h4>
 			</div>
 		</a>
 		<a href="index.php?page=Подарочные коробки">
-			<div class="increase">
+			<div class="increase_low increase">
 				<h4>Подарочные коробки</h4>
 			</div>
 		</a>
--- someCategory.php
+++ someCategory.php
@@ -1,47 +1,26 @@
 <main class="categoryMain">
-	<h1></h1>
+	<h1>Какой-то заголовок</h1>
 	<div class="categoryItems">
+		<?php for ($i=0; $i <= 7; $i++) { 
+		echo '
 		<a href="#">
-			<div>
+		<div>
 			<div class="imgWrap">
-				<img src="images/shop/items/1.jpg" alt="">
+				<img src="images/shop/items/1.jpg" alt="" class="increase_big">
 			</div>
 			<p>Тестовый какой то текст</p>
 			<p>260,00 р.</p>
-			<button>В корзину</button>
+			<div class="buttonWrap">
+				<button class="categoryElement">В корзину </button>
+			</div>
 		</div>
-		</a>	
+		</a>	';
+		} ?>
+		
 
-		<a href="#">
-			<div>
-			<div class="imgWrap">
-				<img src="images/shop/items/2.jpg" alt="">
-			</div>
-			<p>Тестовый какой то текст</p>
-			<p>260,00 р.</p>
-			<button>В корзину</button>
-		</div>
-		</a>	
-
-		<a href="#">
-			<div>
-			<div class="imgWrap">
-				<img src="images/shop/items/3.jpg" alt="">
-			</div>
-			<p>Тестовый какой то текст</p>
-			<p>260,00 р.</p>
-			<button>В корзину</button>
-		</div>
-		</a>	
-
-		<a href="#">
-			<div>
-			<div class="imgWrap">
-				<img src="images/shop/items/4.jpg" alt="">
-			</div>
-			<p>Тестовый какой то текст</p>
-			<p>260,00 р.</p>
-			<button>В корзину</button>
-		</div>
-		</a>	
+	</div>
+	<div class="bottomButtons">
+		<a href="/index.php?page=search" class="searchLink"><div class="searchButton"><img src="/images/shop/search.png" title="Поиск"><p>Поиск</p></div></a>
+		<a href="#" class="basketLink"><div class="basketButton"><img src="/images/shop/shop-basket.png" title="Корзина"><p>Корзина</p></div></a>
+	</div>
 </main>
