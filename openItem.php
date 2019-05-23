ca3895c19d72a306137ce2e16751e6055c424e9a
avisrara74 <avisrara74@gmail.com>
2019-05-23 12:59:04 +0500

    arrow-fix

--- openItem.php
+++ openItem.php
@@ -2,14 +2,14 @@
 	<div class="openItemWrap">
 		<div class="imagesWrap">
 			<div class="mainImg">
-				<div href="#" class="btn-arrow-left" id="prev"></div>
+				<div href="#" class="btn-arrow-left" id="prev">&#10094</div>
 				<div>
 					<img src="images/shop/items/1-1.jpg" alt="" class="slide">
 					<img src="images/shop/items/1-2.jpg" alt="" class="slide displayNone">
 					<img src="images/shop/items/1-3.jpg" alt="" class="slide displayNone">
 					<img src="images/shop/items/1-4.jpg" alt="" class="slide displayNone">
 				</div>
-				<div href="#" class="btn-arrow-right" id="next"></div>
+				<div href="#" class="btn-arrow-right" id="next">&#10095</div>
 				<script  src="js/func.js"></script>
 			</div>
 		</div>
